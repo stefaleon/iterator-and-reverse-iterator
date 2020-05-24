591bfb5582e126e037e59cd2321500ec13416829
stefaleon <stefaleon@gmail.com>
2020-05-24 09:31:21 +0300

    refactor for DRY

--- app.js
+++ app.js
@@ -41,38 +41,27 @@ document.getElementById('previous').addEventListener('click', previousProfile);
 function nextProfile() {
   const currentProfile = profiles.next().value;
 
-  if (currentProfile !== undefined) {
-    document.getElementById('profileDisplay').innerHTML = `
-      <ul class="list-group">
-        <li class="list-group-item">Name: ${currentProfile.name}</li>
-        <li class="list-group-item">Age: ${currentProfile.age}</li>
-        <li class="list-group-item">Location: ${currentProfile.location}</li>
-        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-      </ul>
-    `;
-
-    document.getElementById(
-      'imageDisplay'
-    ).innerHTML = `<img src="${currentProfile.image}">`;
-  } else {
-    // No more profiles
-    window.location.reload();
-  }
+  renderTemplate(currentProfile);
 }
 
 // Previous Profile Display
 function previousProfile() {
   const currentProfile = profiles2.previous().value;
 
+  renderTemplate(currentProfile);
+}
+
+// Render the template
+function renderTemplate(currentProfile) {
   if (currentProfile !== undefined) {
     document.getElementById('profileDisplay').innerHTML = `
-      <ul class="list-group">
-        <li class="list-group-item">Name: ${currentProfile.name}</li>
-        <li class="list-group-item">Age: ${currentProfile.age}</li>
-        <li class="list-group-item">Location: ${currentProfile.location}</li>
-        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
-      </ul>
-    `;
+    <ul class="list-group">
+      <li class="list-group-item">Name: ${currentProfile.name}</li>
+      <li class="list-group-item">Age: ${currentProfile.age}</li>
+      <li class="list-group-item">Location: ${currentProfile.location}</li>
+      <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+    </ul>
+  `;
 
     document.getElementById(
       'imageDisplay'
